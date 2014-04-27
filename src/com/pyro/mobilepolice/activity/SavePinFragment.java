5f3ea88284c15795f1d1fb7dc811df0fb956203d
nanduanil <nanduanil@gmail.com>
2014-04-28 01:52:59 +0530

    Add save pin fragment layout and class

---
+++ src/com/pyro/mobilepolice/activity/SavePinFragment.java
@@ -0,0 +1,22 @@
+package com.pyro.mobilepolice.activity;
+
+import com.pyro.mobilepolice.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SavePinFragment extends Fragment {
+
+	 @Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) {
+	 
+	        View view =  inflater.inflate(R.layout.fragment_save_pin, container, false);
+	        return view;
+	 
+	    }
+}
