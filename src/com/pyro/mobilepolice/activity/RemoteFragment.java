bc3c128007f5714307e7386dc5f3339fa31c9132
Suraj Narayanan <surajx@gmail.com>
2014-06-22 02:38:31 +0530

    Adding integration for remote features

--- src/com/pyro/mobilepolice/activity/RemoteFragment.java
+++ src/com/pyro/mobilepolice/activity/RemoteFragment.java
@@ -2,7 +2,6 @@ package com.pyro.mobilepolice.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +11,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.pyro.mobilepolice.R;
+import com.pyro.mobilepolice.constants.Const;
+import com.pyro.mobilepolice.utils.Utils;
 
 public class RemoteFragment extends Fragment {
 
@@ -32,12 +33,6 @@ public class RemoteFragment extends Fragment {
 		super.onActivityCreated(savedInstanceState);
 		Bundle args = getArguments();
 		funcSelected = args.getString("func");
-		String fromPhoneHeader = getResources().getString(
-				R.string.frag_remote_phone_header);
-		String toPhoneHeader = getResources().getString(
-				R.string.frag_target_phone_header);
-		String pinHeader = getResources().getString(
-				R.string.frag_remote_pin_header);
 
 		final TextView tvFromPhone = (TextView) getActivity().findViewById(
 				R.id.lostPhoneHeader);
@@ -47,53 +42,58 @@ public class RemoteFragment extends Fragment {
 				R.id.pinNumberHeader);
 		Button btnInitiate = (Button) getActivity().findViewById(
 				R.id.btnInitiate);
-		EditText etFromPhone = (EditText) getActivity().findViewById(
+		final EditText etFromPhone = (EditText) getActivity().findViewById(
 				R.id.lostPhoneNumber);
-		EditText etToPhone = (EditText) getActivity().findViewById(
+		final EditText etToPhone = (EditText) getActivity().findViewById(
 				R.id.toPhoneNumber);
-		EditText etPin = (EditText) getActivity().findViewById(R.id.pinNumber);
-		final String smsMessage;
-
-		// String htmlFromPhoneHeader = "<u>" + fromPhoneHeader + "</u>";
-		// String htmlToPhoneHeader = "<u>" + toPhoneHeader + "</u>";
-		// String htmlPinHeader = "<u>" + pinHeader + "</u>";
-
-		String htmlFromPhoneHeader = fromPhoneHeader;
-		String htmlToPhoneHeader = toPhoneHeader;
-		String htmlPinHeader = pinHeader;
-
-		tvFromPhone.setText(Html.fromHtml(htmlFromPhoneHeader));
-		tvToPhone.setText(Html.fromHtml(htmlToPhoneHeader));
-		tvPin.setText(Html.fromHtml(htmlPinHeader));
+		final EditText etPin = (EditText) getActivity().findViewById(
+				R.id.pinNumber);
+		TextView mFuncDescription = (TextView) getActivity().findViewById(
+				R.id.funcDescription);
 
 		if (funcSelected.equals(getResources().getString(
 				R.string.navRingPhoneText))) {
 			etToPhone.setVisibility(View.GONE);
 			tvToPhone.setVisibility(View.GONE);
 			btnInitiate.setText("Ring Phone");
-
+			mFuncDescription.setText(R.string.ring_phone_desc);
 			btnInitiate.setOnClickListener(new View.OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
-					Toast.makeText(getActivity(), "ring phone sms sent",
-							Toast.LENGTH_SHORT).show();
+					String requestMessage = Const.MISSION_REQUEST_IDENTIFIER
+							+ " " + etPin.getText().toString() + " "
+							+ Const.MISSION_IDENTIFIER_RING;
+					Utils.sendSMS(etFromPhone.getText().toString(),
+							requestMessage);
+					Toast.makeText(getActivity(),
+							"Request SMS to Play Ringtone Sent.",
+							Toast.LENGTH_LONG).show();
 				}
 			});
 		} else if (funcSelected.equals(getResources().getString(
 				R.string.navCallBackText))) {
+			mFuncDescription.setText(R.string.init_call_desc);
 			btnInitiate.setText("Call Back");
 
 			btnInitiate.setOnClickListener(new View.OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
-					Toast.makeText(getActivity(), "call back sms sent",
-							Toast.LENGTH_SHORT).show();
+					String requestMessage = Const.MISSION_REQUEST_IDENTIFIER
+							+ " " + etPin.getText().toString() + " "
+							+ Const.MISSION_IDENTIFIER_MAKE_CALL + " "
+							+ etToPhone.getText().toString();
+					Utils.sendSMS(etFromPhone.getText().toString(),
+							requestMessage);
+					Toast.makeText(getActivity(),
+							"Remote Call Initiation Request SMS Sent.",
+							Toast.LENGTH_LONG).show();
 				}
 			});
 		} else if (funcSelected.equals(getResources().getString(
 				R.string.navGetLocationText))) {
+			mFuncDescription.setText(R.string.get_location_desc);
 			etToPhone.setVisibility(View.GONE);
 			tvToPhone.setVisibility(View.GONE);
 			btnInitiate.setText("Get Location");
@@ -102,22 +102,35 @@ public class RemoteFragment extends Fragment {
 
 				@Override
 				public void onClick(View v) {
-					Toast.makeText(getActivity(), "get location sms sent",
-							Toast.LENGTH_SHORT).show();
+					String requestMessage = Const.MISSION_REQUEST_IDENTIFIER
+							+ " " + etPin.getText().toString() + " "
+							+ Const.MISSION_IDENTIFIER_LOCATE;
+					Utils.sendSMS(etFromPhone.getText().toString(),
+							requestMessage);
+					Toast.makeText(getActivity(),
+							"SMS Request to Retrieve Location Sent.",
+							Toast.LENGTH_LONG).show();
 
 				}
 			});
 		} else if (funcSelected.equals(getResources().getString(
 				R.string.navCallForwardText))) {
-
+			mFuncDescription.setText(R.string.call_fwd_desc);
 			btnInitiate.setText("Start Call Forward");
 
 			btnInitiate.setOnClickListener(new View.OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
-					Toast.makeText(getActivity(), "call back sms sent",
-							Toast.LENGTH_SHORT).show();
+					String requestMessage = Const.MISSION_REQUEST_IDENTIFIER
+							+ " " + etPin.getText().toString() + " "
+							+ Const.MISSION_IDENTIFIER_START_CALL_FWD + " "
+							+ etToPhone.getText().toString();
+					Utils.sendSMS(etFromPhone.getText().toString(),
+							requestMessage);
+					Toast.makeText(getActivity(),
+							"Call Forwarding Request SMS Sent.",
+							Toast.LENGTH_LONG).show();
 				}
 			});
 		}
